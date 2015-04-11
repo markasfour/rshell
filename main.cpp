51e0649ad06c78f2c9276172168b4f63d6977be4
Mark Asfour <masfo001@hammer.cs.ucr.edu>
2015-04-11 12:23:55 -0700

    reorganized execvp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include <unistd.h> 
-#include <perror>
+#include <stdio.h>
 #include <vector>
 #include <boost/tokenizer.hpp>
 using namespace std;
@@ -9,10 +9,16 @@ using namespace boost;
 
 int main(int argc, char **argv)
 {
-	//string command;
-	//cout << "$ ";
-	//cin >> command;
-	
-	execvp(command, argv);
+	for(int i = 0; i < argc; i++)
+	{
+		cout << "argv[" << i << "] = " << argv[i] << endl;
+	}
+	cout << "listed the arguements" << endl;
+	for(int i = 1; i < argc; i++)
+	{
+		if(-1 == execvp(argv[i], argv))
+       			perror("There was an error in execvp. ");
+		execvp(argv[i], argv);
+	}
 	return 0;
 }
