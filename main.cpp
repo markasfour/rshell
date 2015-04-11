cbc4e5d545020c4a43e83f49e1d9bcd63030a4b9
Mark Asfour <masfo001@hammer.cs.ucr.edu>
2015-04-11 11:47:48 -0700

    set up execvp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
+#include <string>
+#include <unistd.h> 
+#include <perror>
+#include <vector>
+#include <boost/tokenizer.hpp>
 using namespace std;
+using namespace boost;
 
 int main(int argc, char **argv)
 {
-	for(int i = 0; i < argc; i++)
-	{
-		cout << "argv[" << i << "] = " << argv[i] << endl;
-	}
+	//string command;
+	//cout << "$ ";
+	//cin >> command;
+	
+	execvp(command, argv);
 	return 0;
 }
